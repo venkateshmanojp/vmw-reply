9cc4a20cd12b42b3f67b6d028b51534aad9fba7f
venkateshmanojp <venkateshmanojp@gmail.com>
2026-03-28 02:57:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import express from "express";
-import fetch from "node-fetch";
+const express = require("express");
+const fetch = require("node-fetch");
+
 
 const app = express();
 app.use(express.json());
