1ce84c8342ac68a2aa4720fed00245015a97fced
venkateshmanojp <venkateshmanojp@gmail.com>
2026-04-14 17:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ app.get("/webhook", (req, res) => {
 async function sendTemplateWithImage(to, templateName, imageUrl) {
   try {
     const response = await fetch(
-      `https://graph.facebook.com/v18.0/${PHONE_NUM_ID}/messages`,
+      `https://graph.facebook.com/v18.0/${PHONE_NUMBER_ID}/messages`,
       {
         method : "POST",
         headers: {
